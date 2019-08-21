ad824c9909c90ccbd35663db6751cf53359854f6
kyle.randolph <kyle.randolph@essenceglobal.com>
2019-08-20 21:00:59 -0400

    wrap helpful APIs in class for organization

---
+++ rtf_utils/dfa_utils.py
@@ -0,0 +1,107 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#   Some helpful wrappers for dfa reporting.
+#   RTF - Kyle.Randolph@essenceglobal.com
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+from googleapiclient import discovery, http
+from oauth2client import client
+from io import FileIO
+import logging
+import csv
+
+
+"""
+TODO:
+ - Implement Module Level Logging
+ - Report Cleaning
+
+"""
+
+class CampaignManagerReport:
+    api_name = 'dfareporting'
+    api_version = 'v3.3'
+    api_scopes = ['https://www.googleapis.com/auth/dfareporting', 'https://www.googleapis.com/auth/dfatrafficking',  'https://www.googleapis.com/auth/ddmconversions']
+    
+    def __init__(self,creds,profile_id,report_id):
+        self.profile_id = profile_id
+        self.report_id = report_id        
+        scoped_creds = creds.with_scopes(self.api_scopes)
+        self.service = discovery.build(self.api_name, self.api_version, credentials=scoped_creds,cache_discovery=False)
+        self.report = self.get_report(profile_id,report_id)        
+        self.file = self.get_last_file(profile_id,report_id)        
+        self.file_id = self.file['id']
+    
+    def get_report(self,profile_id,report_id):
+        request = self.service.reports().get(profileId=profile_id,reportId=report_id)
+        resp = request.execute()
+        return resp
+    
+    def get_last_file(self,profile_id,report_id):
+        request = self.service.reports().files().list(profileId=profile_id,reportId=report_id,maxResults=1)
+        resp = request.execute()
+        return resp['items'][0]
+    
+    def run_report(self):
+        request = self.service.reports().run(profileId=self.profile_id,reportId=self.report_id)
+        resp = request.execute()
+        self.file = resp        
+    
+    def patch_report(self,patched_report):
+        request = self.service.reports().patch(profileId=self.profile_id,reportId=self.report_id, body=patched_report)
+        resp = request.execute()
+        self.report = resp
+        
+    def set_date_range(self,start_date,end_date):
+        self.report['criteria']['dateRange'] = {"startDate":start_date,"endDate":end_date,"relativeDateRange":None}        
+        self.patch_report(self.report)
+        
+    def get_report_status(self):
+        request = self.service.files().get(reportId=self.report_id,fileId=self.file['id'])
+        resp = request.execute()
+        self.file = resp
+        self.status = self.file['status']
+    
+    def download_file(self):
+        CHUNK_SIZE = 32 * 1024 * 1024
+        request = self.service.files().get(reportId=self.report_id,fileId=self.file_id)
+        report_file = request.execute()
+
+        file_name = report_file['fileName'] or report_file['id']
+
+        if report_file['format'] == 'CSV': 
+            extension = '.csv' 
+        else:
+            extension = '.xml'
+
+        file_name = file_name + extension
+
+        if report_file['status'] == 'REPORT_AVAILABLE':
+            out_file = FileIO(file_name, mode='wb')
+
+            request = self.service.files().get_media(reportId=self.report_id, fileId=self.file_id)
+
+            downloader = http.MediaIoBaseDownload(out_file, request,
+                                                chunksize=CHUNK_SIZE)
+
+            download_finished = False
+
+            while download_finished is False:
+                _, download_finished = downloader.next_chunk()
+        return file_name
+
+
+def clean_dcm_file(csvobj):
+    data = []
+    write = False
+    reader = csv.reader(csvobj, delimiter=',')
+    for row in reader:
+        if write == True:
+            data.append(row)        
+        elif row == ['Report Fields']:
+            write = True
+
+    if data[-1][0] == 'Grand Total:':
+        data.pop()
+    return data
+
+## Implement saving cleaned file ^^
---
+++ rtf_utils/gcp_utils.py
@@ -0,0 +1,135 @@
+from googleapiclient import discovery, http
+from oauth2client import client
+from google.cloud import bigquery, storage
+import logging
+
+
+############ GCS Utils ############
+
+
+def list_blobs(bucket_name,prefix=None):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    if prefix:
+        blobs = bucket.list_blobs(prefix=prefix)
+    else:
+        blobs = bucket.list_blobs()
+    return blobs
+
+def upload_blob(bucket_name, destination_blob_name, source, mode):
+    """Uploads a file to the bucket."""
+    if mode not in ["string","filename"]:
+        raise SyntaxError    
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    
+    if mode == "string":
+        blob.upload_from_string(source)
+    elif mode == "filename":
+        blob.upload_from_filename(source)
+    
+    print('File uploaded as {}.'.format(destination_blob_name))
+
+def copy_blob(bucket_name, blob_name, new_bucket_name, new_blob_name):
+    """Copies a blob from one bucket to another with a new name."""
+    storage_client = storage.Client()
+    source_bucket = storage_client.get_bucket(bucket_name)
+    source_blob = source_bucket.blob(blob_name)
+    destination_bucket = storage_client.get_bucket(new_bucket_name)
+
+    new_blob = source_bucket.copy_blob(
+        source_blob, destination_bucket, new_blob_name)
+
+    print('Blob {} in bucket {} copied to blob {} in bucket {}.'.format(
+        source_blob.name, source_bucket.name, new_blob.name,
+        destination_bucket.name))
+
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.delete()
+
+    print('Blob {} deleted.'.format(blob_name))
+
+def get_blob(bucket,blob):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket)
+    blob = bucket.get_blob(blob)
+    return blob
+
+
+############ BQ Utils ############
+
+def bq_query(query):
+    client = bigquery.Client()
+    '''
+    query = (
+        "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
+        'WHERE state = "TX" '
+        "LIMIT 100"
+    )
+    '''
+    
+    query_job = client.query(
+        query,
+        # Location must match that of the dataset(s) referenced in the query.
+        location="US",
+    )  # API request - starts the query
+
+    for row in query_job:  # API request - fetches results
+        # Row values can be accessed by field name or index
+        assert row[0] == row.name == row["name"]
+        print(row)
+
+def bq_load(dataset_id,file_uri,schema,dest_table,filetype,mode=None):
+    ## switch to logging
+    ## uri = "gs://cloud-samples-data/bigquery/us-states/us-states.json"
+    
+    client = bigquery.Client()
+    dataset_ref = client.dataset(dataset_id)
+    
+    ## Job Config
+    job_config = bigquery.LoadJobConfig()    
+    
+    if mode == "Append":
+        job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+    
+    if mode == "Overwrite":
+        job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
+
+    else:
+        job_config.write_disposition = bigquery.WriteDisposition.WRITE_EMPTY
+    
+    ## build bq schema from list of dicts
+    bq_schema = []
+    
+    for col in schema:
+        bq_schema.append(bigquery.SchemaField(col.get('name'),col.get('type')))
+    
+    job_config.schema = bq_schema
+    
+    if filetype == "json":
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+    
+    elif filetype == "csv":
+        job_config.source_format = bigquery.SourceFormat.CSV
+        job_config.skip_leading_rows = 1
+
+    else:
+        raise NotImplementedError
+    
+    load_job = client.load_table_from_uri(
+        file_uri,
+        dataset_ref.table(dest_table),
+        location="US",  # Location must match that of the destination dataset.
+        job_config=job_config,
+    )
+    logging.info("Starting job {}".format(load_job.job_id))
+
+    load_job.result()  # Waits for table load to complete.
+    logging.info("Job finished.")
