ccf29d3c1129a1bf4ef084efb4a9f8d2b077cef0
kfrandolph90 <kyle.randolph@essenceglobal.com>
2019-09-13 17:38:14 -0400

    bad, rushed code

--- brand_reporting_dags/dfa_reporting_dag.py
+++ brand_reporting_dags/dfa_reporting_dag.py
@@ -13,18 +13,15 @@ default_args = {
     'start_date': datetime(2019, 9, 1), 
     'retries': 3, 
     'retry_delay': timedelta(minutes=5),
-    'execution_timeout': timedelta(minutes=60),
-    'provide_context':True, # this makes xcom work
-    'write_disposition': 'WRITE_EMPTY',
-    'create_disposition': 'CREATE_IF_NEEDED',
-    'project_id':'essence-analytics-dwh',
-    'staging_bucket':'rtf_staging'
+    'execution_timeout': timedelta(minutes=20),
+    'provide_context':True,# this makes xcom work
+    'catchup':False
 }
 
 dag = DAG('Google-Brand-Reporting_CampaignManager',
             default_args=default_args,
             description='Move DataLab Export to Staging GCS',
-            schedule_interval=None, 
+            schedule_interval='0 6 * * *', 
             catchup=False)
 
 ## load tasks
@@ -33,42 +30,51 @@ with open('/home/airflow/gcs/dags/RTF/brand_reporting_dags/brand_report_task_que
     campaigns = config_json['campaigns']
 
 ## build dcm tasks
-campaign_manager_tasks = []
-for campaign in campaigns:
-    campaign_manager_tasks.extend(campaign['dcmTasks'])
-
-print(campaign_manager_tasks)
-
-
 start_task = DummyOperator(task_id="Start", retries=0, dag=dag)
 end_task = DummyOperator(task_id="End", retries=0, dag=dag)
 
+for i,campaign in enumerate(campaigns):
 
-for task in campaign_manager_tasks:
-    report_name = task['name']
-    report_id = task['reportId']
-
-    task_id_base = "Report-" + str(report_id)
+    campaign_task = DummyOperator(task_id="Campaign_{}".format(i+1), retries=0, dag=dag)
     
-    req_id  = task_id_base + "-extract"
+    campaign_manager_tasks = campaign['dcmTasks']
+    start_task.set_downstream(campaign_task)
 
-    request_task = PythonOperator(task_id=req_id,
-                                    python_callable=dfa_report_extract, 
-                                    op_args=[report_id],
-                                    dag=dag, provide_context=True)
-    
-    load_id  = task_id_base + "-load"
-    dataset_table = "RTF_DWH_CampaignManager."+ "STAGING_" + report_name
-    
-    load_bq_task = PythonOperator(task_id=load_id,
-                                    python_callable=dfa_report_load, 
-                                    op_args=[req_id,dataset_table],
-                                    dag=dag, provide_context=True)
+    for task in campaign_manager_tasks:
+        report_name = task['name']
+        report_id = task['reportId']
+
+        task_id_base = "Report-" + str(report_id)
+        
+        req_id  = task_id_base + "-extract"
+
+        request_task = PythonOperator(task_id=req_id,
+                                        python_callable=dfa_report_extract, 
+                                        op_args=[report_id],
+                                        dag=dag, provide_context=True)
+        
+        load_id  = task_id_base + "-load"
+        
+       
+        if task.get('dest'):
+            dest = task['dest']
+
+        else:
+            dest = ("RTF_DWH_CampaignReporting." + 
+            task['name'] + "_" +
+            datetime.now().strftime('%Y%m%d'))
+        
+        load_bq_task = PythonOperator(task_id=load_id,
+                                        python_callable=dfa_report_load, 
+                                        op_args=[req_id,dest],
+                                        dag=dag, provide_context=True)
 
 
-    
-    cleaning_tasks = PythonOperator(task_id=task_id_base + "_clean", python_callable=clean_up, op_args=[req_id],dag=dag,provide_context=True)
-    
-    start_task >> request_task >> load_bq_task >> cleaning_tasks >> end_task
+        
+        cleaning_tasks = PythonOperator(task_id=task_id_base + "_clean", python_callable=clean_up, op_args=[req_id],dag=dag,provide_context=True)
+        
+        campaign_task >> request_task >> load_bq_task >> cleaning_tasks
 
+        end_task.set_upstream(cleaning_tasks)
 
+        
